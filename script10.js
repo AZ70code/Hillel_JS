be09bef81150e20a736d23ee8630d551ca62632f
AZ70code <alexzubov1970@gmail.com>
2022-02-23 11:00:21 +0200

    add comments

--- script10.js
+++ script10.js
@@ -12,9 +12,10 @@ scene.addEventListener("mousemove", function (e) {
 	if (!ballActive) return;
 
 	const { clientX, clientY } = e;
-	let left = clientX - sceneRect.left - BORDER_WIDTH - ballRect.width / 2;
-	let top = clientY - sceneRect.top - BORDER_WIDTH - ballRect.height / 2;
+	let left = clientX - sceneRect.left - BORDER_WIDTH - ballRect.width / 2;// обьявляем переменную
+	let top = clientY - sceneRect.top - BORDER_WIDTH - ballRect.height / 2;// let вместо const
 
+//============ homework ===========================================
 	if(left < 0) left = 0;
 	if(top < 0) top = 0;
 	if(left > scene.clientWidth - BORDER_WIDTH * 2 - ballRect.width) {
@@ -23,6 +24,7 @@ scene.addEventListener("mousemove", function (e) {
 	if(top > scene.clientHeight - BORDER_WIDTH * 2 - ballRect.height) {
 		top = scene.clientHeight - BORDER_WIDTH * 2 - ballRect.height;
 	}
+//=======================================================================
 	ball.style.transform = `translate(${left}px, ${top}px)`;
 });
 
