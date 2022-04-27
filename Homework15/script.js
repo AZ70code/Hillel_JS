cc63cf1d5fe9bcbd1688a1e6f5d5bbb6e5847d66
AZ70code <alexzubov1970@gmail.com>
2022-04-27 15:09:13 +0300

    new solution

--- Homework15/script.js
+++ Homework15/script.js
@@ -1,19 +1,16 @@
+
 function createAsyncList(iterations) {
-    const data = [];
-
-    function dataCreation(n) {
-        let obj = {};
-        if(n>0){
-            obj.massage = 'Current iteration'+' '+`${n}`;
-            dataCreation(n-1);
-            data.push(obj);
-        } else return;
-    }
-
-    return new Promise(resolve => {
-        dataCreation(iterations);
-        resolve(data);
-    })
+    const arr = [];
+    return function fn() {
+        return new Promise ((resolve, reject) => {
+            setTimeout (() => {
+                arr.push({message: `Current iteration ${iterations}`})
+                iterations--;
+                if(iterations !=0) resolve(fn());
+                else resolve(arr.reverse());
+            }, 1000)
+        })
+    }()
 }
 createAsyncList(3).then((data) => {
 console.log(data);
